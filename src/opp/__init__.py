9f1a0915c0703cb2cac2db83fb8e194de634a5bf
Renanzai <renanzai@example.com>
2026-05-27 15:55:11 +0800

    chore: bump version to 0.5.6

--- src/opp/__init__.py
+++ src/opp/__init__.py
@@ -1,6 +1,6 @@
 """OPP - Omni Pre-Processor: Document content extraction package."""
 
-__version__ = "0.5.5"
+__version__ = "0.5.6"
 
 from opp.extractors.docx import DOCXExtractor
 from opp.extractors.pptx import PPTXExtractor
